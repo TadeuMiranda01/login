6deaad74b8d9b161e3073532a61c786f81dd5bad
TadeuMiranda01 <tadeumiranda0003@gmail.com>
2023-01-26 11:22:03 -0300

    initial commit

--- material-tic-tac-toe/src/app/app.component.spec.ts
+++ material-tic-tac-toe/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'login'`, () => {
+  it(`should have as title 'material-tic-tac-toe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('login');
+    expect(app.title).toEqual('material-tic-tac-toe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('login app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('material-tic-tac-toe app is running!');
   });
 });
